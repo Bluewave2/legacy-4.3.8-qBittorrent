032b0f3431fac1f463116790b04a8d7c5c0d0e95
Bluewave2 <83724034+Bluewave2@users.noreply.github.com>
2023-06-28 14:19:22 +0200

    Fixed missing svg

--- compile.bat
+++ compile.bat
@@ -1,2 +1,2 @@
-python make-resource2.py -find-files  -config config.json -icons-dir icons -output legacy-4.3.8-qBittorrent-bluewave2-v1.17 -style style.qss
+python make-resource2.py -find-files  -config config.json -icons-dir icons -output legacy-4.3.8-qBittorrent-bluewave2-v1.18 -style style.qss
 pause
